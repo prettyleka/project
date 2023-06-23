ea6bbbfd993afc18361b95869a4890485ed2ba3c
Valeria Basov <prettyleka@gmail.com>
2023-06-24 02:10:43 +0300

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
-$(function () {
-    $('.counter').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    })
-}); 
+const counterUp = window.counterUp.default
+
+const el = document.querySelectorAll( '.counter' )
+
+el.forEach(element => {
+    counterUp(element)
+});
 
 
 $(function() {
@@ -22,5 +16,36 @@ $(function() {
 $(function() {
     $("#signupButton").click(function(){
         $("#signupModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#contact-cat").click(function(){
+        $("#telephoneModal-cat").modal("show");
+    });
+});
+$(function() {
+    $("#contact-cat1").click(function(){
+        $("#telephoneModal-cat").modal("show");
+    });
+});
+$(function() {
+    $("#contact-cat2").click(function(){
+        $("#telephoneModal-cat").modal("show");
+    });
+});
+$(function() {
+    $("#contact-dog").click(function(){
+        $("#telephoneModal-dog").modal("show");
+    });
+});
+$(function() {
+    $("#contact-dog1").click(function(){
+        $("#telephoneModal-dog").modal("show");
+    });
+});
+$(function() {
+    $("#contact-dog2").click(function(){
+        $("#telephoneModal-dog").modal("show");
     });
 });
